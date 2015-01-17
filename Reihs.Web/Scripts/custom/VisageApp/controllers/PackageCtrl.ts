b0a5b76217fb2ad8f01c5cebbf9e8e130d9ba542
andreij6 <andreij6@gmail.com>
2015-01-17 16:44:16 -0500

    PackagCtrl Added

---
+++ Reihs.Web/Scripts/custom/VisageApp/controllers/PackageCtrl.ts
@@ -0,0 +1,20 @@
+﻿module VisageApp {
+	export class PackageCtrl {
+		private $scope: Extensions.IPackageScope;
+
+		private init(): void {
+			var self = this;
+		}
+
+		constructor($scope: Extensions.IPackageScope) {
+			var self = this;
+			self.$scope = $scope;
+
+			self.$scope.test = "About Page";
+
+			self.init();
+		}
+	}
+
+	PackageCtrl.$inject = ['$scope'];
+}
