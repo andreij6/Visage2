3f44b5413c64ce1e7fac5943e76537e2be7f1e50
alljones <acreativej@gmail.com>
2015-02-03 21:57:52 -0600

    Added Paypal ID to Repository

--- Visage.Repository/Seeder.cs
+++ Visage.Repository/Seeder.cs
@@ -61,142 +61,170 @@ namespace Reihs.Repository
 					new mProduct {
 						Name = "Lytera® Skin Brightening Complex",
 						Brand = "SkinMedica",
-						UnitPrice = 125.0
+						UnitPrice = 125.0,
+                        PayPalId = "C8GPXCKXK7JAA"
 					},
 					new mProduct {
 						Name = "AHA / BHA Cleanser",
 						Brand = "SkinMedica",
-						UnitPrice = 46.0
+						UnitPrice = 46.0,
+                        PayPalId = "9CMV7RRVWCCAA"
 					},
 					new mProduct {
 						Name = "AHA/BHA Cream",
 						Brand = "SkinMedica",
-						UnitPrice = 42.0
+						UnitPrice = 42.0,
+                        PayPalId = "886KF2LBAZCSS"
 					},
 					new mProduct {
 						Name = "Daily Physical Defense® Sunscreen Broad Spectrum SPF 30+",
 						Brand = "SkinMedica",
-						UnitPrice = 46.0
+						UnitPrice = 46.0,
+                        PayPalId = "L5V73ZL3CPR4E"
 					},
 					new mProduct {
 						Name = "Dermal Repair Cream",
 						Brand = "SkinMedica",
-						UnitPrice = 124.0
+						UnitPrice = 124.0,
+                        PayPalId = "TMMZML8T7UH8N"
 					},
 					new mProduct {
 						Name = "Environmental Defense Sunscreen™ SPF 50+ with UV ProPlex™",
 						Brand = "SkinMedica",
-						UnitPrice = 46.0
+						UnitPrice = 46.0,
+                        PayPalId = "XEGEL2NVP8DGC"
 					},
 					new mProduct {
 						Name = "Facial Cleanser",
 						Brand = "SkinMedica",
-						UnitPrice = 36.0
+						UnitPrice = 36.0,
+                        PayPalId = "38STWVQQLDJMA"
 					},
 					new mProduct {
 						Name = "Hydrating Complex",
 						Brand = "SkinMedica",
-						UnitPrice = 84.0
+						UnitPrice = 84.0,
+                        PayPalId = "WW2CDDQBK8QDL"
 					},
 					new mProduct {
 						Name = "Purifying Foaming Wash",
 						Brand = "SkinMedica",
-						UnitPrice = 42.0
+						UnitPrice = 42.0,
+                        PayPalId = "FEE6PDEGSYD3S"
 					},
 					new mProduct {
 						Name = "Redness Relief CalmPlex",
 						Brand = "SkinMedica",
-						UnitPrice = 84.0
+						UnitPrice = 84.0,
+                        PayPalId = "ZG34BPMJSGPXE"
 					},
 					new mProduct {
 						Name = "Rejuvenative Moisturizer",
 						Brand = "SkinMedica",
-						UnitPrice = 56.0
+						UnitPrice = 56.0,
+                        PayPalId = "4VESWU62UYLCJ"
 					},
 					new mProduct {
 						Name = "Rejuvenative Toner",
 						Brand = "SkinMedica",
-						UnitPrice = 36.0
+						UnitPrice = 36.0,
+                        PayPalId = "8FBNWSENXFLK6"
 					},
 					new mProduct {
 						Name = "Retinol Complex .25",
 						Brand = "SkinMedica",
-						UnitPrice = 60.0
+						UnitPrice = 60.0,
+                        PayPalId = "VL7WJ37J5GMX6"
 					},
 					new mProduct {
 						Name = "Retinol Complex 0.5",
 						Brand = "SkinMedica",
-						UnitPrice = 75.0
+						UnitPrice = 75.0,
+                        PayPalId = "ZWDJPNDRZZTU6"
 					},
 					new mProduct {
 						Name = "Retinol Complex 1.0",
 						Brand = "SkinMedica",
-						UnitPrice = 90.0
+						UnitPrice = 90.0,
+                        PayPalId = "8HAVECYSMFUQ"
 					},
 					new mProduct {
 						Name = "Scar Recovery Gel with Centelline™ (0.5 Oz.)",
 						Brand = "SkinMedica",
-						UnitPrice = 42.0
+						UnitPrice = 42.0,
+                        PayPalId = "V4LUU2VZ8GDUQ"
 					},
 					new mProduct {
 						Name = "Scar Recovery Gel with Centelline™ (2 Oz.)",
 						Brand = "SkinMedica",
-						UnitPrice = 98.0
+						UnitPrice = 98.0,
+                        PayPalId = "DQ98XZFGS5RLS"
 					},
 					new mProduct {
 						Name = "Sensitive Skin Cleanser",
 						Brand = "SkinMedica",
-						UnitPrice = 36.0
+						UnitPrice = 36.0,
+                        PayPalId = "39P93MBJ2BRM2"
 					},
 					new mProduct {
 						Name = "TNS Ceramide Treatment Cream™",
 						Brand = "SkinMedica",
-						UnitPrice = 66.0
+						UnitPrice = 66.0,
+                        PayPalId = "3XPTCKR9KR3H4"
 					},
 					new mProduct {
 						Name = "TNS Essential Serum®",
 						Brand = "SkinMedica",
-						UnitPrice = 270.0
+						UnitPrice = 270.0,
+                        PayPalId = "JJ3KRHYFXS722"
 					},
 					new mProduct {
 						Name = "TNS Eye Repair™",
 						Brand = "SkinMedica",
-						UnitPrice = 98.0
+						UnitPrice = 98.0,
+                        PayPalId = "WVB9WJB6PWYNJ"
 					},
 					new mProduct {
 						Name = "TNS Illuminating Eye Cream®",
 						Brand = "SkinMedica",
-						UnitPrice = 88.0
+						UnitPrice = 88.0,
+                        PayPalId = "73EDPAZLQXVNC"
 					},
 					new mProduct {
 						Name = "TNS Lip Plump System®",
 						Brand = "SkinMedica",
-						UnitPrice = 58.0
+						UnitPrice = 58.0,
+                        PayPalId = "XJJ7HWH8P6FP2"
 					},
 					new mProduct {
 						Name = "TNS Recovery Complex®",
 						Brand = "SkinMedica",
-						UnitPrice = 172.0
+						UnitPrice = 172.0,
+                        PayPalId = "24AWZEVSBZUVE"
 					},
 					new mProduct {
 						Name = "TNS Ultimate Daily Moisturizer™ SPF 20 Sunscreen",
 						Brand = "SkinMedica",
-						UnitPrice = 88.0
+						UnitPrice = 88.0,
+                        PayPalId = "KEH9G9EJSAW6W"
 					},
 					new mProduct {
 						Name = "Ultra Sheer Moisturizer",
 						Brand = "SkinMedica",
-						UnitPrice = 56.0
+						UnitPrice = 56.0,
+                        PayPalId = "SVCA7TEVK3MCE"
 					},
 					new mProduct {
 						Name = "Uplifting Eye Serum™",
 						Brand = "SkinMedica",
-						UnitPrice = 58.0
+						UnitPrice = 58.0,
+                        PayPalId = "RNFMP5QRGXP68"
 					},
 					new mProduct {
 						Name = "Vitamin C+E Complex",
 						Brand = "SkinMedica",
-						UnitPrice = 98.0
+						UnitPrice = 98.0,
+                        PayPalId = "HQSJR49JEZRAY"
 					}
 
 				);
@@ -210,27 +238,32 @@ namespace Reihs.Repository
 					new mProduct {
 						Name = "Intense Moisturizer",
 						Brand = "Elta MD",
-						UnitPrice = 14.5
+						UnitPrice = 14.5,
+                        PayPalId = "23VUY55JF8QQU"
 					},
 					new mProduct {
 						Name = "UV Clear 46 SPF",
 						Brand = "Elta MD",
-						UnitPrice = 37.0
+						UnitPrice = 37.0,
+                        PayPalId = "JSRYJAWF7WTK8"
 					},
 					new mProduct {
 						Name = "UV Daily 40 SPF",
 						Brand = "Elta MD",
-						UnitPrice = 30
+						UnitPrice = 30.0,
+                        PayPalId = "HC5XP4S54CJCL"
 					},
 					new mProduct {
 						Name = "UV Physical 41",
 						Brand = "Elta MD",
-						UnitPrice = 36.0
+						UnitPrice = 36.0,
+                        PayPalId = "BLTSS3PDPVUQL"
 					},
 					new mProduct {
 						Name = "UV Shield 45 SPF",
 						Brand = "Elta MD",
-						UnitPrice = 36.0
+						UnitPrice = 36.0,
+                        PayPalId = "CCNBJLUW2GGRS"
 					}
 			);
 
@@ -243,7 +276,8 @@ namespace Reihs.Repository
 				new mProduct {
 					Name = "RevitaLash Advanced Formula",
 					Brand = "RevitaLash",
-					UnitPrice = 120.0
+					UnitPrice = 120.0,
+                    PayPalId = "E9G58TQK3UYAY"
 				});
 
 			context.SaveChanges();
@@ -255,23 +289,33 @@ namespace Reihs.Repository
 				new mProduct {
 					Name = "Clarisonic Mia",
 					Brand = "Clarisonic",
-					UnitPrice = 125.0
+					UnitPrice = 125.0,
+                    PayPalId = "S73DYTU3DGURJ"
 				},
 				new mProduct {
 					Name = "Clarisonic Pro",
 					Brand = "Clarisonic",
-					UnitPrice = 195.0
+					UnitPrice = 195.0,
+                    PayPalId = "8VXQ6Q9GGKU36"
 				},
 				new mProduct {
 					Name = "Clarisonic Pro System Kit",
 					Brand = "Clarisonic",
-					UnitPrice = 195.0
+					UnitPrice = 195.0,
+                    PayPalId = "FT6U7TE7T6LF4"
 				},
 				new mProduct {
 					Name = "Clarisonic Pro System Plus Body",
 					Brand = "Clarisonic",
-					UnitPrice = 250.0
-				});
+					UnitPrice = 250.0,
+                    PayPalId = "Z56ZFFNW2C8RE"
+				},
+                new mProduct
+                {
+                    Name = "Gift Card",
+                    UnitPrice = 0.0,
+                    PayPalId = "SJ22FXJAFF9RL"
+                });
 
 			context.SaveChanges();
 		}
