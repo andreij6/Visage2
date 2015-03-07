d048beda210c78738de07ace6c3d6df207aeafe2
andreij6 <andreij6@gmail.com>
2015-03-07 05:54:50 -0500

    some css/html work on videos and home page

--- Reihs.Web/Scripts/custom/VisageApp/controllers/VideoCtrl.ts
+++ Reihs.Web/Scripts/custom/VisageApp/controllers/VideoCtrl.ts
@@ -59,15 +59,19 @@
 			var nav = '../../../Templates/Front/Videos/Partials/'
 			self.$scope.Videos = [
 				{ Id: 1, Title: 'Botox & Restylane Patient Testimonial', Template: nav + 'BotoxTestimonial.html' },
-				{ Id: 2, Title: 'Testimonial: Botox, Juvederm, BodyFX', Template: nav + 'Testimonialbjb.html'},
-				{ Id: 3, Title: 'Intense Pulsed Light (IPL)', Template: nav + 'IPL.html'},
-				{ Id: 4, Title: 'Botox Demo', Template: nav + 'BotoxDemo.html'},
-				{ Id: 5, Title: 'BodyFX', Template: nav + 'BodyFX.html'},
-				{ Id: 6, Title: 'Skin Medica', Template: nav + 'SkinMedica.html'},
-				{ Id: 7, Title: 'TSN Essential', Template: nav + 'TSN.html'},
-				{ Id: 8, Title: 'Services and Products Overview', Template: nav + 'Overview.html'},
+				{ Id: 2, Title: 'Testimonial: Botox, Juvederm, BodyFX', Template: nav + 'Testimonialbjb.html' },
+				{ Id: 3, Title: 'Intense Pulsed Light (IPL)', Template: nav + 'IPL.html' },
+				{ Id: 4, Title: 'Botox Demo', Template: nav + 'BotoxDemo.html' },
+				{ Id: 5, Title: 'BodyFX', Template: nav + 'BodyFX.html' },
+				{ Id: 6, Title: 'Skin Medica', Template: nav + 'SkinMedica.html' },
+				{ Id: 7, Title: 'TSN Essential', Template: nav + 'TSN.html' },
+				{ Id: 8, Title: 'Services and Products Overview', Template: nav + 'Overview.html' },
 				{ Id: 9, Title: 'Botox', Template: nav + 'Botox.html' },
 				{ Id: 10, Title: 'Sclerotherapy', Template: nav + 'Sclerotherapy.html' },
+				{ Id: 11, Title: 'Nancy - Customer Testimonial', Template: nav + 'NancyTestimonial.html'},
+				{ Id: 12, Title: 'Lynn - Customer Testimonial', Template: nav + 'LynnTestimonial.html'},
+				{ Id: 13, Title: 'Kim - Customer Testimonial', Template: nav + 'KimTestimonial.html'},
+				{ Id: 14, Title: 'Skin Medica 2', Template: nav + 'SkinMedica_2.html'}
 			]
 			self.$scope.Video = {};
 
