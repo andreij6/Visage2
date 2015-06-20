2318381838454932b609aefce93b60a437873295
alljones <acreativej@gmail.com>
2015-06-20 14:47:56 -0500

    Added Nancy Testimontial and Team Positions added

--- Reihs.Web/Scripts/custom/VisageApp/controllers/TeamCtrl.ts
+++ Reihs.Web/Scripts/custom/VisageApp/controllers/TeamCtrl.ts
@@ -39,7 +39,7 @@
 				"Physician",
 				"Owner and Full-time Physician of Le Beau Visage.",
 				true,
-				"Jason", "Riehs", 1);
+				"Jason M.", "Riehs, MD JD", 1);
 
 			self.$scope.TeamMembers.push(drReihs);
 
@@ -70,8 +70,8 @@
 			self.$scope.TeamMembers.push(Jenny);
 
 			var RachelG = new Staffer(
-				'Medical Assitant',
-				'I am the Physicians Coordinator at Le Beau Visage.  I’m an Orange County girl, raised in Saudi Arabia, and settled in Texas.  I am currently attending Collin County Community College and later plan to earn a Bachelor of Science in Nursing from the University of North Texas.  My patients are my favorite part of my day.  When I’m not working, I love traveling, crafting, and being outdoors.  If you ever need a sushi date or a meaty steak, I’m your girl!',
+				'Patient Coordinator',
+				'I’m an Orange County girl, raised in Saudi Arabia, and settled in Texas.  I am currently attending Collin County Community College and later plan to earn a Bachelor of Science in Nursing from the University of North Texas.  My patients are my favorite part of my day.  When I’m not working, I love traveling, crafting, and being outdoors.  If you ever need a sushi date or a meaty steak, I’m your girl!',
 				false,
 				'Rachel', 'Gonzalez', 5
 				);
@@ -88,7 +88,7 @@
 			self.$scope.TeamMembers.push(Jordan);
 
 			var Carla = new Staffer(
-				"Registered Nurse",
+				"HCG Registered Nurse",
 				"Carla is a registered nurse with more than two decades experience in patient care and health education.  She graduated with her nursing degree in 1990 and recently received her RNC national certification.  Carla has a passion for helping people look and feel their best.  She has assisted Dr. Reihs for the last 4 years mentoring clients through the medically supervised HCG weight loss program at LeBeau Visage Medical Spa.  She has resided in Frisco, Texas since 1995 with her husband, where they have raised their 3 children and now enjoys their beautiful granddaughter.  Her favorite pastimes are playing the piano, golf, cooking, and adapting recipes to promote a more healthy lifestyle.",
 				false,
 				"Carla", "Marchand", 7
@@ -106,7 +106,7 @@
 			self.$scope.TeamMembers.push(Suzanna);
 
 			var Lauren = new Staffer(
-				"Front Office",
+				"Adminstrator",
 				"I was born and raised in Dallas, Texas.  I graduated top of the class from Paul Mitchell The School and have been part of this amazing industry for over 10 years.  Making people look and feel better about themselves is a truly rewarding experience that never gets old.  Thank you for letting me be a daily part of your life and here’s to many more years to come!",
 				false,
 				"Lauren", "Wheeler", 9
@@ -115,7 +115,7 @@
 			self.$scope.TeamMembers.push(Lauren);
 
 			var Monet = new Staffer(
-				"Front Office",
+				"Adminstrator",
 				"Transplanted to Texas in 1996 by my husband’s job transferred, I have two sons who are both in college. Once our nest was empty, I decided to pursue a job in the health care field.  I obtained my Medical Secretary Certificate from Collin College in 2014.  I am also a Certified Administrative Health Assistant with the National Association for Health Professionals scoring a 96% on the exam.  Dr. Riehs’ mission statement of providing an outstanding patient experience resonated with me and I strive to make that happen.  In addition to travelling with my husband, I enjoy reading, gardening, exercising and my delightful Yorkie Scooter.",
 				false,
 				"Monet", "Thompson", 10
@@ -130,7 +130,7 @@
 	export class Staffer {
 
 		constructor(
-			public Postion,
+			public Position,
 			public Biography,
 			public ReadMore,
 			public FirstName,
