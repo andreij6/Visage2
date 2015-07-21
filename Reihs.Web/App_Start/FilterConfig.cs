c2e9f8de80fe169284eb5a46998313a35e916979
andreij6 <andreij6@gmail.com>
2015-07-20 23:56:36 -0400

    removed global https filter

--- Reihs.Web/App_Start/FilterConfig.cs
+++ Reihs.Web/App_Start/FilterConfig.cs
@@ -9,7 +9,7 @@ namespace Reihs.Web
 		{
 			filters.Add(new HandleErrorAttribute());
 			filters.Add(new AuthorizeAttribute());
-			filters.Add(new RequireHttpsAttribute());
+			//filters.Add(new RequireHttpsAttribute());
 		}
 	}
 }
